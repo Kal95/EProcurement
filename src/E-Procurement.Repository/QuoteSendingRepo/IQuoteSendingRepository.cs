405e54342297ca4e466c2bfd3d2fcc6fa5deab0f
gabyslaw <gabyslaw@gmail.com>
2019-06-13 16:50:54 +0100

    Added Quoute sending repo

---
+++ src/E-Procurement.Repository/QuoteSendingRepo/IQuoteSendingRepository.cs
@@ -0,0 +1,11 @@
+﻿using E_Procurement.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Procurement.Repository.QuoteSendingRepo
+{
+    public interface IQuoteSendingRepository : IDependencyRegister
+    {
+    }
+}
---
+++ src/E-Procurement.Repository/QuoteSendingRepo/QuoteSendingRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Procurement.Repository.QuoteSendingRepo
+{
+    public class QuoteSendingRepository
+    {
+    }
+}
