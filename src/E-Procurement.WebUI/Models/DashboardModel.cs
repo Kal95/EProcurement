741bbffbe9fb8e763b5252a827aa5e7ae4011655
Mr. Gray <graham.onwuegbuzie@cyberspace.net.ng>
2019-08-08 12:06:10 +0100

    Updated DashBoard

---
+++ src/E-Procurement.WebUI/Models/DashboardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Procurement.WebUI.Models
+{
+    public class DashboardModel
+    {
+        public string RegVen { get; set; }
+        public string PO { get; set; }
+        public string RFQ { get; set; }
+    }
+}
